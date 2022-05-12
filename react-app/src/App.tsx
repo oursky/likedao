1caf446785a8744c4d7bd3e7cce71eeac07a2d5b
Rico Wong <hochiw@oursky.com>
2022-05-13 02:39:45 +0800

    react-app: Setup sample error page for error fallback
    refs #13

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import * as Sentry from "@sentry/react";
 import AppRouter from "./navigation/AppRouter";
 import AppProviders from "./providers/AppProviders";
 
 const App: React.FC = () => {
   return (
     <AppProviders>
-      <AppRouter />
+      {/* TODO: Handle fallback error screen */}
+      <Sentry.ErrorBoundary fallback={<p>Error</p>}>
+        <AppRouter />
+      </Sentry.ErrorBoundary>
     </AppProviders>
   );
 };
