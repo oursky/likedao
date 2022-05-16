a0629a6f183542b2b79ad34357eacf303b67bc8b
Rico <hochiw@oursky.com>
2022-05-16 15:43:40 +0800

    graphql-server: Implement latest block resolver
    refs #3

---
+++ graphql-server/pkg/resolvers/block.go
@@ -0,0 +1,26 @@
+package resolvers
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	pkgContext "github.com/oursky/likedao/pkg/context"
+	graphql1 "github.com/oursky/likedao/pkg/generated/graphql"
+	"github.com/oursky/likedao/pkg/models"
+)
+
+func (r *queryResolver) GetLatestBlock(ctx context.Context) (*models.Block, error) {
+	res, err := pkgContext.GetQueriesFromCtx(ctx).Block.QueryLatestBlock()
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+
+}
+
+// Query returns graphql1.QueryResolver implementation.
+func (r *Resolver) Query() graphql1.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
--- graphql-server/pkg/resolvers/test.go
+++ graphql-server/pkg/resolvers/test.go
@@ -33,8 +33,4 @@ func (r *queryResolver) QueryTestByID(ctx context.Context, id string) (*models.T
 // Mutation returns graphql1.MutationResolver implementation.
 func (r *Resolver) Mutation() graphql1.MutationResolver { return &mutationResolver{r} }
 
-// Query returns graphql1.QueryResolver implementation.
-func (r *Resolver) Query() graphql1.QueryResolver { return &queryResolver{r} }
-
 type mutationResolver struct{ *Resolver }
-type queryResolver struct{ *Resolver }
