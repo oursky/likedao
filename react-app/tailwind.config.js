1203b00a29ed3e44973e03da4c09b29ead565bfd
Rico Wong <hochiw@oursky.com>
2022-05-18 01:23:58 +0800

    react-app: Implement AppNavigationMenu and MenuItems
    refs #33

--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     extend: {
       colors: {
         likecoin: {
+          green: "#28646E",
+          lightgreen: "#A5B6B9",
+          grey: "#D8D8D8",
           "primary-bg": "#F8F7F4",
+          gradient: {
+            from: "#D7EFE7",
+            to: "#EFE7B8",
+          },
         },
       },
     },
