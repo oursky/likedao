8e7bf8a242eba1dc69218d288d82e4710efe2323
Rico <hochiw@oursky.com>
2022-05-17 15:23:31 +0800

    react-app: Move feature demo to dummy screen
    refs #33

--- react-app/src/navigation/AppRouter.tsx
+++ react-app/src/navigation/AppRouter.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DummyScreen from "../components/DummyScreen/DummyScreen";
-import HomeScreen from "../components/HomeScreen/HomeScreen";
 import AppRoutes from "./AppRoutes";
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoutes.Home} element={<HomeScreen />} />
         <Route path={AppRoutes.Dummy} element={<DummyScreen />} />
-        <Route path="*" element={<HomeScreen />} />
       </Routes>
     </BrowserRouter>
   );
