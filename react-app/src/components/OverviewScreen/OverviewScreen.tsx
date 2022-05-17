b43a64b280dc8d1adfa05e721f2c745eed545f69
Rico Wong <hochiw@oursky.com>
2022-05-18 01:25:02 +0800

    react-app: Integrate NavigationMenu to create sidebar for overview page
    refs #33

--- react-app/src/components/OverviewScreen/OverviewScreen.tsx
+++ react-app/src/components/OverviewScreen/OverviewScreen.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import cn from "classnames";
+import { useLocation } from "react-router-dom";
+import AppNavigationMenu from "../AppNavigationMenu/AppNavigationMenu";
+
+import Divider from "../common/Divider/Divider";
+import LocalizedText from "../common/Localized/LocalizedText";
+import { ReactComponent as LikeLogo } from "../../assets/likecoin-logo.svg";
 
 const OverviewScreen: React.FC = () => {
+  const location = useLocation();
+
   return (
     <div
       className={cn(
@@ -17,10 +25,64 @@ const OverviewScreen: React.FC = () => {
       <div
         className={cn("h-full", "flex", "flex-row", "gap-4", "justify-center")}
       >
-        <div className={cn("flex-0", "w-72", "bg-red-400")}>
-          <p>SideBar</p>
+        <div
+          className={cn(
+            "flex-0",
+            "flex",
+            "flex-row",
+            "gap-y-4",
+            "sm:w-72",
+            "sm:flex-col"
+          )}
+        >
+          <div
+            className={cn(
+              "flex-1",
+              "flex",
+              "flex-row",
+              "gap-x-4",
+              "items-center",
+              "sm:items-start",
+              "sm:flex-col",
+              "sm:gap-y-4",
+              "sm:gap-x-0"
+            )}
+          >
+            <LikeLogo height={48} width={48} />
+            <h1
+              className={cn(
+                "text-base",
+                "leading-5",
+                "font-normal",
+                "text-likecoin-green"
+              )}
+            >
+              <LocalizedText messageID="AppSideBar.title" />
+            </h1>
+            {/* TODO: Implement chain switcher */}
+            <div className={cn("h-5", "bg-pink-400", "text-sm")}>
+              Chain Switcher
+            </div>
+          </div>
+          {/* TODO: Handle login dialog  */}
+          <div
+            className={cn(
+              "py-2",
+              "h-[92px]",
+              "bg-pink-400",
+              "hidden",
+              "sm:block"
+            )}
+          >
+            Login
+          </div>
+          <Divider />
+          <AppNavigationMenu
+            activeRoute={location.pathname}
+            className={cn("hidden", "sm:flex")}
+          />
         </div>
-        <div className={cn("flex-1", "bg-blue-400", "max-w-5xl")}>
+        <div className={cn("flex-1", "bg-blue-400", "max-w-screen-2xl")}>
           <p>Main Content</p>
         </div>
       </div>
