98963e3d36a796bd7680da492db426d7f91dd513
Rico Wong <hochiw@oursky.com>
2022-05-20 03:09:53 +0800

    react-app: Implement Wallet base class and Keplr wallet client
    refs #41

---
+++ react-app/src/clients/keplrClient.ts
@@ -0,0 +1,27 @@
+import { OfflineSigner } from "@cosmjs/proto-signing";
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { ChainInfo } from "../config/Config";
+import { BaseWallet } from "./baseWallet";
+
+export class KeplrWallet extends BaseWallet {
+  static async connect(chainInfo: ChainInfo): Promise<KeplrWallet> {
+    const keplrClient = window.keplr;
+    await keplrClient.enable(chainInfo.chainId);
+
+    const offlineSigner = window.getOfflineSigner(
+      chainInfo.chainId
+    ) as OfflineSigner;
+
+    const cosmJS = await SigningStargateClient.connectWithSigner(
+      chainInfo.chainRpc,
+      offlineSigner
+    );
+
+    return new KeplrWallet(chainInfo, offlineSigner, cosmJS);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async disconnect(): Promise<void> {
+    console.log("No action");
+  }
+}
