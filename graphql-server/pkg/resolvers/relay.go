c6b511e94d7b18455e248a4af6cb75810ec192d4
Rico <hochiw@oursky.com>
2022-05-19 17:09:38 +0800

    graphql-server: Implement generic node query
    refs #29

---
+++ graphql-server/pkg/resolvers/relay.go
@@ -0,0 +1,23 @@
+package resolvers
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/oursky/likedao/pkg/models"
+)
+
+func (r *queryResolver) Node(ctx context.Context, id models.NodeID) (models.Node, error) {
+	switch id.EntityType {
+	case "test":
+		return r.QueryTestByID(ctx, id)
+	default:
+		panic(fmt.Sprintf(
+			`unknown entity type "%s"`,
+			id.EntityType,
+		))
+	}
+}
