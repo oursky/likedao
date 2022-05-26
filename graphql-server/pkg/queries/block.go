1a6842889e0e24f923679d0e2f181ba64fe65687
Rico <hochiw@oursky.com>
2022-05-26 14:14:08 +0800

    graphql-server: Create batch block query
    refs #50

--- graphql-server/pkg/queries/block.go
+++ graphql-server/pkg/queries/block.go
@@ -5,12 +5,14 @@ import (
 	"strings"
 
 	"github.com/oursky/likedao/pkg/models"
+	"github.com/pkg/errors"
 	"github.com/uptrace/bun"
 )
 
 type IBlockQuery interface {
 	QueryLatestBlock() (*models.Block, error)
 	QueryBlockByHash(hash string) (*models.Block, error)
+	QueryBlocksByHashes(hashes []string) ([]*models.Block, error)
 }
 
 type BlockQuery struct {
@@ -40,4 +42,32 @@ func (q *BlockQuery) QueryBlockByHash(hash string) (*models.Block, error) {
 	}
 
 	return block, nil
+}
+
+func (q *BlockQuery) QueryBlocksByHashes(hashes []string) ([]*models.Block, error) {
+	blocks := make([]*models.Block, 0, len(hashes))
+	uppercasedHashes := make([]string, 0, len(hashes))
+	for _, hash := range hashes {
+		uppercasedHashes = append(uppercasedHashes, strings.ToUpper(hash))
+	}
+	if err := q.session.NewSelect().Model(&blocks).Where("hash IN (?)", bun.In(uppercasedHashes)).Scan(q.ctx); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	result := make([]*models.Block, 0, len(blocks))
+	hashToBlock := make(map[string]models.Block, len(blocks))
+	for _, block := range blocks {
+		hashToBlock[block.Hash] = *block
+	}
+
+	for _, hash := range uppercasedHashes {
+		block, exists := hashToBlock[hash]
+		if exists {
+			result = append(result, &block)
+		} else {
+			result = append(result, nil)
+		}
+	}
+
+	return result, nil
 }
