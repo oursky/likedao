126aaca01af2880d4ade1cd4da94b358571ae3e4
Rico Wong <hochiw@oursky.com>
2022-05-28 12:00:33 +0800

    react-app: Implement scalar parser for BigInt and BigFloat
    refs #50

--- react-app/src/clients/apolloClient.ts
+++ react-app/src/clients/apolloClient.ts
@@ -2,15 +2,63 @@ import {
   ApolloClient,
   InMemoryCache,
   NormalizedCacheObject,
+  ApolloLink,
+  HttpLink,
 } from "@apollo/client";
+import { withScalars } from "apollo-link-scalars";
+import BigNumber from "bignumber.js";
+import {
+  buildClientSchema,
+  GraphQLScalarType,
+  IntrospectionQuery,
+} from "graphql";
 import Config from "../config/Config";
+import introspection from "../generated/graphql.schema.json";
 
 const typePolicies = {};
+const scalars: Record<string, GraphQLScalarType> = {
+  BigInt: new GraphQLScalarType({
+    name: "BigInt",
+    serialize: (parsed: unknown): string | null => {
+      return parsed instanceof BigNumber ? parsed.toFixed() : null;
+    },
+    parseValue: (value: unknown): BigNumber | null => {
+      if (!value) return null;
+      if (typeof value === "string") {
+        return new BigNumber(value);
+      }
+
+      throw new Error(`Cannot parse value as BigInt`);
+    },
+  }),
+  BigFloat: new GraphQLScalarType({
+    name: "BigFloat",
+    serialize: (parsed: unknown): string | null => {
+      return parsed instanceof BigNumber ? parsed.toFixed() : null;
+    },
+    parseValue: (value: unknown): BigNumber | null => {
+      if (!value) return null;
+      if (typeof value === "string") {
+        return new BigNumber(value);
+      }
+
+      throw new Error(`Cannot parse value as BigFloat`);
+    },
+  }),
+};
+
+const schema = buildClientSchema(
+  introspection as unknown as IntrospectionQuery
+);
+
+const link = ApolloLink.from([
+  withScalars({ schema, typesMap: scalars }),
+  new HttpLink({ uri: Config.graphql.endpoint }),
+]);
 
 export const createApolloClient = (): ApolloClient<NormalizedCacheObject> => {
-  const config = Config.graphql;
   return new ApolloClient({
-    uri: config.endpoint,
+    link,
     defaultOptions: {
       query: {
         fetchPolicy: "cache-first",
