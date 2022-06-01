4e4a25322d76f7f260901fbf9580013a6775c14c
Rico Wong <hochiw@oursky.com>
2022-06-01 13:02:36 +0800

    react-app: Create proposals page
    refs #68

---
+++ react-app/src/components/ProposalScreen/ProposalScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import cn from "classnames";
+
+const ProposalScreen: React.FC = () => {
+  return (
+    <div className={cn("flex-1", "bg-white", "rounded-lg")}>
+      <p>Main Content</p>
+    </div>
+  );
+};
+
+export default ProposalScreen;
--- react-app/src/navigation/AppRoutes.ts
+++ react-app/src/navigation/AppRoutes.ts
@@ -1,6 +1,7 @@
 const AppRoutes = Object.freeze({
   Dummy: "/dummy",
   Overview: "/",
+  Proposals: "/proposals",
 });
 
 export default AppRoutes;
