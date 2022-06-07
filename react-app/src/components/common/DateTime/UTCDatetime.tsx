6d2a53eecc6ebb5856a63028e0b2b6d3f34c938f
ivanng-oursky <ivan.ng@oursky.com>
2022-06-07 17:46:35 +0800

    react-app: created Datetime component refs oursky/likedao#87

---
+++ react-app/src/components/common/DateTime/UTCDatetime.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { format } from "date-fns";
+
+const UTCDatetime: React.FC<{ date: Date; className?: string }> = ({
+  date,
+  className = "",
+}) => {
+  return (
+    <time className={className} dateTime={date.toISOString()}>
+      {format(date.toISOString().slice(0, -1), "YYYY-mm-DD HH:mm")} UTC
+    </time>
+  );
+};
+
+export default UTCDatetime;
