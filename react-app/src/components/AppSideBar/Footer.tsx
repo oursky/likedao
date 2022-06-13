b68b9e721f93191d54b69470c71abc7d7d6a4349
ivanng-oursky <ivan.ng@oursky.com>
2022-06-13 16:42:42 +0800

    react-app: fix localization & update link - use localiztion instead of hard code text - update get token from ATOM token instead of UST - use dynamic year for copyright
    refs oursky/likedao#100

--- react-app/src/components/AppSideBar/Footer.tsx
+++ react-app/src/components/AppSideBar/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import cn from "classnames";
 import Divider from "../common/Divider/Divider";
 import AppButton from "../common/Buttons/AppButton";
+import LocalizedText from "../common/Localized/LocalizedText";
 
 const Footer: React.FC = () => {
   return (
@@ -20,8 +21,13 @@ const Footer: React.FC = () => {
           "sm:mt-5"
         )}
       >
-        <p className={cn("sm:grow", "sm:text-left")}>© 2022 Likecoin</p>
-        <p className={cn("mt-6", "sm:mt-0")}>Get Token</p>
+        <p className={cn("sm:grow", "sm:text-left")}>
+          © {new Date().getFullYear()}{" "}
+          <LocalizedText messageID="footer.likecoin" />
+        </p>
+        <p className={cn("mt-6", "sm:mt-0")}>
+          <LocalizedText messageID="footer.getToken" />
+        </p>
         <div
           className={cn(
             "flex",
@@ -36,7 +42,7 @@ const Footer: React.FC = () => {
             theme="secondary"
             size="small"
             type="anchor"
-            href="https://app.osmosis.zone/?from=UST&to=LIKE"
+            href="https://app.osmosis.zone/?from=ATOM&to=LIKE"
             className={cn(
               "text-sm",
               "leading-4",
@@ -83,7 +89,9 @@ const Footer: React.FC = () => {
             "sm:px-3"
           )}
         >
-          <p className={cn("pr-3")}>Need help?</p>
+          <p className={cn("pr-3")}>
+            <LocalizedText messageID="footer.needHelp" />
+          </p>
           <AppButton
             messageID="footer.contactSupport"
             theme="secondary"
