a2cbc03fa910b47a796a0ed99ff0f4d489016860
ivanng-oursky <ivan.ng@oursky.com>
2022-06-14 17:59:53 +0800

    react-app: move footer links to config refs oursky/likedao#100

--- react-app/config/config.template.js
+++ react-app/config/config.template.js
@@ -15,4 +15,11 @@ window.appConfig = {
     endpoint: "http://localhost:8080/graphql",
   },
   chainLinks: [],
+  footerLinks: {
+    osmosis: "https://app.osmosis.zone/?from=ATOM&to=LIKE",
+    liquid: "https://app.liquid.com/exchange/LIKEUSDT",
+    emeris: "https://app.emeris.com/welcome",
+    contactSupport:
+      "https://go.crisp.chat/chat/embed/?website_id=5c009125-5863-4059-ba65-43f177ca33f7",
+  },
 };
--- react-app/src/config/Config.ts
+++ react-app/src/config/Config.ts
@@ -17,6 +17,13 @@ export interface ChainInfo {
   };
 }
 
+interface FooterLinks {
+  osmosis: string;
+  liquid: string;
+  emeris: string;
+  contactSupport: string;
+}
+
 export interface IConfig {
   sentry: Pick<
     Sentry.BrowserOptions,
@@ -27,6 +34,7 @@ export interface IConfig {
     endpoint: string;
   };
   chainLinks: ChainLink[];
+  footerLinks: FooterLinks;
 }
 
 const defaultConfig: IConfig = {
@@ -46,6 +54,13 @@ const defaultConfig: IConfig = {
     endpoint: "http://localhost:8080/graphql",
   },
   chainLinks: [],
+  footerLinks: {
+    osmosis: "https://app.osmosis.zone/?from=ATOM&to=LIKE",
+    liquid: "https://app.liquid.com/exchange/LIKEUSDT",
+    emeris: "https://app.emeris.com/welcome",
+    contactSupport:
+      "https://go.crisp.chat/chat/embed/?website_id=5c009125-5863-4059-ba65-43f177ca33f7",
+  },
 };
 
 const appConfig = window.appConfig;
