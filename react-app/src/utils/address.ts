68b7360f8646c61e358ed2016ec6927e3b747b82
ivanng-oursky <ivan.ng@oursky.com>
2022-06-15 11:41:09 +0800

    react-app: implement translateAddress function refs oursky/likedao#131

--- react-app/src/utils/address.ts
+++ react-app/src/utils/address.ts
@@ -1,3 +1,5 @@
+import { fromBech32, normalizeBech32, toBech32 } from "@cosmjs/encoding";
+
 const ADDRESS_DIVIDER = "1";
 
 export function truncateAddress(address: string): string {
@@ -11,4 +13,17 @@ export function truncateAddress(address: string): string {
   return `${prefix}${ADDRESS_DIVIDER}...${addressSuffix.slice(
     addressSuffix.length - 4
   )}`;
+}
+
+/**
+ * Translate bech32 address from one encoding to another
+ * @param address - encoding bech32 address
+ * @param prefix - target prefix of the output address
+ * @returns translated bech32 address
+ */
+export function translateAddress(address: string, prefix: string): string {
+  const normalizedAddress = normalizeBech32(address);
+  const decodedAddress = fromBech32(normalizedAddress);
+  const encodedAddress = toBech32(prefix, decodedAddress.data);
+  return encodedAddress;
 }
