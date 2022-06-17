ee1a350c2658b7427238ef41d88dc35dd2c80169
Rico <hochiw@oursky.com>
2022-06-17 17:57:39 +0800

    graphql-server: Implement user proposal reaction dataloader
    refs #115

--- graphql-server/pkg/dataloaders/reaction.go
+++ graphql-server/pkg/dataloaders/reaction.go
@@ -2,11 +2,13 @@ package dataloaders
 
 import (
 	godataloader "github.com/cychiuae/go-dataloader"
+	"github.com/oursky/likedao/pkg/models"
 	"github.com/oursky/likedao/pkg/queries"
 )
 
 type ReactionDataloader interface {
 	LoadProposalReactionCount(id int) (queries.ReactionCounts, error)
+	LoadUserProposalReactions(key UserProposalReactionKey) (*models.Reaction, error)
 }
 
 type ProposalReactionCountDataloader interface {
@@ -14,8 +16,19 @@ type ProposalReactionCountDataloader interface {
 	LoadAll(ids []int) ([]queries.ReactionCounts, []error)
 }
 
+type UserProposalReactionKey struct {
+	ProposalID  int
+	UserAddress string
+}
+
+type UserProposalReactionDataloader interface {
+	Load(key UserProposalReactionKey) (*models.Reaction, error)
+	LoadAll(keys []UserProposalReactionKey) ([]*models.Reaction, []error)
+}
+
 type IReactionDataloader struct {
 	proposalReactionCountLoader ProposalReactionCountDataloader
+	userProposalReactionLoader  UserProposalReactionDataloader
 }
 
 func NewReactionDataloader(reactionQuery queries.IReactionQuery) ReactionDataloader {
@@ -35,11 +48,56 @@ func NewReactionDataloader(reactionQuery queries.IReactionQuery) ReactionDataloa
 		},
 	})
 
+	userProposalReactionLoader := godataloader.NewDataLoader(godataloader.DataLoaderConfig[UserProposalReactionKey, *models.Reaction]{
+		MaxBatch: DefaultMaxBatch,
+		Wait:     DefaultWait,
+		Fetch: func(keys []UserProposalReactionKey) ([]*models.Reaction, []error) {
+
+			userToProposalIDs := make(map[string][]int)
+			for _, key := range keys {
+				if _, ok := userToProposalIDs[key.UserAddress]; !ok {
+					userToProposalIDs[key.UserAddress] = []int{}
+				}
+				userToProposalIDs[key.UserAddress] = append(userToProposalIDs[key.UserAddress], key.ProposalID)
+			}
+
+			keyToReaction := make(map[UserProposalReactionKey]*models.Reaction)
+			keyToErrors := make(map[UserProposalReactionKey]error)
+			for user, proposalIDs := range userToProposalIDs {
+				userProposalReactions, err := reactionQuery.ScopeProposals(proposalIDs).ScopeUserAddress(user).QueryUserReactions()
+
+				for i, proposalID := range proposalIDs {
+					key := UserProposalReactionKey{
+						ProposalID:  proposalID,
+						UserAddress: user,
+					}
+					keyToReaction[key] = userProposalReactions[i]
+					keyToErrors[key] = err
+				}
+			}
+
+			result := make([]*models.Reaction, 0, len(keys))
+			errors := make([]error, 0, len(keys))
+			for _, key := range keys {
+				result = append(result, keyToReaction[key])
+				errors = append(errors, keyToErrors[key])
+			}
+
+			return result, nil
+
+		},
+	})
+
 	return &IReactionDataloader{
 		proposalReactionCountLoader: proposalReactionCountLoader,
+		userProposalReactionLoader:  userProposalReactionLoader,
 	}
 }
 
 func (d *IReactionDataloader) LoadProposalReactionCount(id int) (queries.ReactionCounts, error) {
 	return d.proposalReactionCountLoader.Load(id)
+}
+
+func (d *IReactionDataloader) LoadUserProposalReactions(key UserProposalReactionKey) (*models.Reaction, error) {
+	return d.userProposalReactionLoader.Load(key)
 }
