a3fd00d2440f1a6109ee2558a6c3baee3c28f925
Rico <hochiw@oursky.com>
2022-06-21 19:10:04 +0800

    react-app: Update color bar to allow showing percentages
    refs #126

--- react-app/src/components/common/ColorBar/ColorBar.tsx
+++ react-app/src/components/common/ColorBar/ColorBar.tsx
@@ -1,34 +1,90 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import cn from "classnames";
+import BigNumber from "bignumber.js";
 
 export interface ColorBarData {
-  value: number;
+  value: BigNumber;
   colorClassName: string;
 }
 
+interface ColorBarSectionProps {
+  data: ColorBarData;
+  total: BigNumber;
+  showPercentage?: boolean;
+}
+
+const ColorBarSection: React.FC<ColorBarSectionProps> = (props) => {
+  const { data, total, showPercentage } = props;
+  const [percentageEl, setPercentageEl] = useState<HTMLSpanElement | null>(
+    null
+  );
+
+  const percentage = useMemo(() => {
+    return data.value.div(total).shiftedBy(2).toFixed(1);
+  }, [data, total]);
+
+  useEffect(() => {
+    if (percentageEl) {
+      const parentEle = percentageEl.parentElement;
+      if (
+        !showPercentage ||
+        (parentEle && parentEle.offsetWidth < percentageEl.offsetWidth)
+      ) {
+        percentageEl.hidden = true;
+      }
+    }
+  }, [percentageEl, showPercentage]);
+
+  return (
+    <div
+      className={cn(
+        "flex",
+        "h-full",
+        "items-center",
+        "justify-center",
+        "overflow-hidden",
+        "flex-col",
+        "flex-wrap",
+        data.colorClassName
+      )}
+      style={{
+        width: `${percentage}%`,
+      }}
+    >
+      <span
+        ref={setPercentageEl}
+        className={cn("text-xs", "leading-5", "font-medium", "text-white")}
+      >
+        {percentage}%
+      </span>
+    </div>
+  );
+};
+
 interface ColorBarProps {
   className?: string;
+  showPercentage?: boolean;
   data: ColorBarData[];
 }
 
 const ColorBar: React.FC<ColorBarProps> = (props) => {
-  const { className, data } = props;
+  const { className, data, showPercentage } = props;
 
   const total = useMemo(
-    () => data.reduce((total, data) => total + data.value, 0),
+    () =>
+      data.reduce((total, data) => total.plus(data.value), new BigNumber(0)),
     [data]
   );
 
   return (
-    <div className={cn("flex", "h-1.5", className)}>
-      {total !== 0 ? (
+    <div className={cn("flex", "h-full", "overflow-hidden", className)}>
+      {!total.eq(0) ? (
         data.map((data, index) => (
-          <div
+          <ColorBarSection
             key={index}
-            className={cn("inline-block", "h-full", data.colorClassName)}
-            style={{
-              width: `${(data.value / total) * 100}%`,
-            }}
+            data={data}
+            total={total}
+            showPercentage={showPercentage}
           />
         ))
       ) : (
