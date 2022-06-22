dcebd3110fc4e1dd05ba890608dc2e894c425117
Rico <hochiw@oursky.com>
2022-06-22 17:54:43 +0800

    react-app: Implement DepositProposalForm
    refs #122

---
+++ react-app/src/components/forms/DepositProposalForm/DepositProposalForm.tsx
@@ -0,0 +1,109 @@
+import React, { useCallback } from "react";
+import cn from "classnames";
+import { useForm } from "react-hook-form";
+import BigNumber from "bignumber.js";
+import AppButton from "../../common/Buttons/AppButton";
+import * as FormField from "../../common/Form";
+import LocalizedText from "../../common/Localized/LocalizedText";
+import Config from "../../../config/Config";
+import {
+  useDepositProposalFromModel,
+  DepositProposalFormValues,
+} from "./DepositProposalFormModel";
+
+interface DepositProposalFormProps {
+  className?: string;
+  proposalId: number;
+  availableTokens: BigNumber;
+  onCancel: () => void;
+  onSubmit: (data: DepositProposalFormValues) => void;
+}
+
+const DepositProposalForm: React.FC<DepositProposalFormProps> = (props) => {
+  const { className, availableTokens, proposalId, onCancel, onSubmit } = props;
+  const coinDenom = Config.chainInfo.currency.coinDenom;
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    clearErrors,
+    formState: { errors },
+  } = useForm<DepositProposalFormValues>({
+    mode: "onSubmit",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      proposalId,
+    },
+  });
+
+  const setAmount = useCallback(
+    (value: string | number) => {
+      clearErrors("amount");
+      setValue("amount", value.toString());
+    },
+    [setValue, clearErrors]
+  );
+
+  const { registerOptions } = useDepositProposalFromModel(availableTokens);
+
+  return (
+    <form
+      className={cn("flex", "flex-col", "gap-y-6", "items-start", className)}
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <FormField.Currency
+        label="DepositProposalModal.fields.amount"
+        currencyUnit={coinDenom}
+        inputProps={{
+          placeholder: "0",
+        }}
+        setValue={setAmount}
+        registerReturn={register("amount", registerOptions.amount)}
+        errorMessage={errors.amount?.message}
+      />
+      <span
+        className={cn(
+          "-mt-5",
+          "text-xs",
+          "leading-6",
+          "font-medium",
+          "text-black"
+        )}
+      >
+        <LocalizedText
+          messageID="DepositProposalModal.fields.amount.available"
+          messageArgs={{
+            amount: availableTokens.toFixed(),
+            denom: coinDenom,
+          }}
+        />
+      </span>
+
+      <FormField.TextInput
+        label="SendTokenModal.fields.memo"
+        registerReturn={register("memo", registerOptions.memo)}
+        errorMessage={errors.memo?.message}
+      />
+
+      <div className={cn("flex", "flex-row", "gap-x-2.5", "self-end")}>
+        <AppButton
+          theme="secondary"
+          size="regular"
+          onClick={onCancel}
+          messageID="TransactionModal.cancel"
+        />
+        <AppButton
+          theme="primary"
+          size="regular"
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onClick={handleSubmit(onSubmit)}
+          messageID="TransactionModal.next"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default DepositProposalForm;
---
+++ react-app/src/components/forms/DepositProposalForm/DepositProposalFormModel.ts
@@ -0,0 +1,63 @@
+import { useMemo } from "react";
+import BigNumber from "bignumber.js";
+
+import { RegisterOptions } from "react-hook-form";
+import { useFormValidators } from "../../../hooks/useFormValidators";
+import { useLocale } from "../../../providers/AppLocaleProvider";
+
+export interface DepositProposalFormValues {
+  proposalId: number;
+  amount: string;
+  memo: string | null;
+}
+
+export const useDepositProposalFromModel = (
+  availableTokens: BigNumber
+): {
+  registerOptions: Record<keyof DepositProposalFormValues, RegisterOptions>;
+} => {
+  const { translate } = useLocale();
+  const { requiredValidator, tokenAmountValidators, maxLengthValidator } =
+    useFormValidators();
+
+  const registerOptions = useMemo((): Record<
+    keyof DepositProposalFormValues,
+    RegisterOptions
+  > => {
+    return {
+      proposalId: {
+        required: requiredValidator,
+      },
+      amount: {
+        required: requiredValidator,
+        validate: {
+          isFinite: tokenAmountValidators.isFinite,
+          isGreaterThanZero: tokenAmountValidators.isGreaterThanZero,
+          isLessThanMax: (v) => {
+            const amount = new BigNumber(v);
+            return (
+              amount.isLessThan(availableTokens) ||
+              translate("form.validation.amount.lessThan", {
+                value: availableTokens.toNumber(),
+              })
+            );
+          },
+        },
+      },
+      memo: {
+        required: false,
+        maxLength: maxLengthValidator(140),
+      },
+    };
+  }, [
+    availableTokens,
+    requiredValidator,
+    tokenAmountValidators,
+    translate,
+    maxLengthValidator,
+  ]);
+
+  return {
+    registerOptions,
+  };
+};
