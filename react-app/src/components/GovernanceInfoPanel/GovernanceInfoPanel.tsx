28ac40db258969f01928e294064bafbcfc15dcc4
ivanng-oursky <ivan.ng@oursky.com>
2022-06-23 16:51:04 +0800

    react-app: implement gov info panel UI refs oursky/likedao#168

--- react-app/src/components/GovernanceInfoPanel/GovernanceInfoPanel.tsx
+++ react-app/src/components/GovernanceInfoPanel/GovernanceInfoPanel.tsx
@@ -2,9 +2,37 @@ import React, { useCallback, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import cn from "classnames";
 import Paper from "../common/Paper/Paper";
-import { GovParams, useGovAPI } from "../../api/govAPI";
+import { useGovAPI } from "../../api/govAPI";
 import { useLocale } from "../../providers/AppLocaleProvider";
 import LoadingSpinner from "../common/LoadingSpinner/LoadingSpinner";
+import LocalizedText from "../common/Localized/LocalizedText";
+import { Icon, IconType } from "../common/Icons/Icons";
+import {
+  convertBigNumberToFixedPointString,
+  formatDecimalToPercentage,
+} from "../../utils/number";
+import { MessageID } from "../../i18n/LocaleModel";
+import { GovParams } from "../../models/cosmos/gov";
+import { convertDurationToDays } from "../../utils/datetime";
+
+const InfoField: React.FC<{
+  labelMessageID: MessageID;
+  value: React.ReactNode;
+}> = ({ labelMessageID, value }) => (
+  <div className="flex flex-col justify-between">
+    <h3
+      className={cn(
+        "text-likecoin-lightgreen",
+        "text-sm",
+        "font-medium",
+        "mb-2"
+      )}
+    >
+      <LocalizedText messageID={labelMessageID} />
+    </h3>
+    <div className={cn("text-base", "leading-5", "font-medium")}>{value}</div>
+  </div>
+);
 
 const GovernanceInfoPanel: React.FC = () => {
   const govAPI = useGovAPI();
@@ -35,9 +63,54 @@ const GovernanceInfoPanel: React.FC = () => {
     );
   }
 
+  const {
+    deposit: { minDeposit, maxDepositPeriod },
+    tally: { quorum, threshold, vetoThreshold },
+    voting: { votingPeriod },
+  } = govParams;
+
   return (
-    <Paper className={cn("")}>
-      <pre>{JSON.stringify(govParams, null, 4)}</pre>
+    <Paper>
+      <div className={cn("flex", "items-center", "mb-5", "gap-2.5")}>
+        <Icon icon={IconType.Info} className={cn("w-6", "h-6")} />
+        <h2 className={cn("text-lg", "leading-none", "font-bold")}>
+          <LocalizedText messageID="GovernanceInfoPanel.governanceInfo" />
+        </h2>
+      </div>
+      <div
+        className={cn("flex", "flex-col", "sm:flex-row", "gap-3", "sm:gap-16")}
+      >
+        <InfoField
+          labelMessageID="GovernanceInfoPanel.minDeposit"
+          value={`${convertBigNumberToFixedPointString(
+            minDeposit.amount
+          )} ${minDeposit.denom.toUpperCase()}`}
+        />
+        <InfoField
+          labelMessageID="GovernanceInfoPanel.maxDepositPeriod"
+          value={translate("GovernanceInfoPanel.period", {
+            days: convertDurationToDays(maxDepositPeriod),
+          })}
+        />
+        <InfoField
+          labelMessageID="GovernanceInfoPanel.quorum"
+          value={`${formatDecimalToPercentage(quorum)}%`}
+        />
+        <InfoField
+          labelMessageID="GovernanceInfoPanel.threshold"
+          value={`${formatDecimalToPercentage(threshold)}%`}
+        />
+        <InfoField
+          labelMessageID="GovernanceInfoPanel.vetoThreshold"
+          value={`${formatDecimalToPercentage(vetoThreshold)}%`}
+        />
+        <InfoField
+          labelMessageID="GovernanceInfoPanel.votingPeriod"
+          value={translate("GovernanceInfoPanel.period", {
+            days: convertDurationToDays(votingPeriod),
+          })}
+        />
+      </div>
     </Paper>
   );
 };
