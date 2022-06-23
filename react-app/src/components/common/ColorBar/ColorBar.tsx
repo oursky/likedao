a64e5c3cb402a1e6ce01ec4410a4989ed53aa894
Rico <hochiw@oursky.com>
2022-06-23 17:25:05 +0800

    react-app: Add tooltip to ColorBar
    refs #126

--- react-app/src/components/common/ColorBar/ColorBar.tsx
+++ react-app/src/components/common/ColorBar/ColorBar.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import cn from "classnames";
 import BigNumber from "bignumber.js";
+import { usePopper } from "react-popper";
 import { useWindowEvent } from "../../../hooks/useWindowEvent";
+import Tooltip from "../Tooltip/Tooltip";
 
 export interface ColorBarData {
   value: BigNumber;
@@ -20,8 +22,16 @@ const ColorBarSection: React.FC<ColorBarSectionProps> = (props) => {
     null
   );
 
+  const [showTooltip, setShowTooltip] = useState<boolean>(false);
+  const [refEle, setRefEle] = useState<HTMLDivElement | null>(null);
+  const [tooltipEle, setTooltipEle] = useState<HTMLDivElement | null>(null);
+
+  const { styles, attributes, update } = usePopper(refEle, tooltipEle, {
+    placement: "bottom",
+  });
+
   const percentage = useMemo(() => {
-    return data.value.div(total).shiftedBy(2).toFixed(1);
+    return `${data.value.div(total).shiftedBy(2).toFixed(1)}%`;
   }, [data, total]);
 
   const handlePercentageDisplay = useCallback(() => {
@@ -36,6 +46,18 @@ const ColorBarSection: React.FC<ColorBarSectionProps> = (props) => {
     }
   }, [percentageEl, showPercentage]);
 
+  const handleMouseEnter = useCallback(() => {
+    if (!showPercentage) return;
+
+    setShowTooltip(true);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    update?.();
+  }, [showPercentage, update]);
+
+  const handleMouseLeave = useCallback(() => {
+    setShowTooltip(false);
+  }, []);
+
   useEffect(() => {
     handlePercentageDisplay();
   }, [handlePercentageDisplay]);
@@ -43,28 +65,43 @@ const ColorBarSection: React.FC<ColorBarSectionProps> = (props) => {
   useWindowEvent("resize", handlePercentageDisplay);
 
   return (
-    <div
-      className={cn(
-        "flex",
-        "h-full",
-        "items-center",
-        "justify-center",
-        "overflow-hidden",
-        "flex-col",
-        "flex-wrap",
-        data.colorClassName
-      )}
-      style={{
-        width: `${percentage}%`,
-      }}
-    >
-      <span
-        ref={setPercentageEl}
-        className={cn("text-xs", "leading-5", "font-medium", "text-white")}
+    <>
+      <div
+        className={cn(
+          "flex",
+          "h-full",
+          "items-center",
+          "justify-center",
+          "overflow-hidden",
+          "flex-col",
+          "flex-wrap",
+          data.colorClassName
+        )}
+        style={{
+          width: percentage,
+        }}
+        ref={setRefEle}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
-        {percentage}%
-      </span>
-    </div>
+        <span
+          ref={setPercentageEl}
+          className={cn("text-xs", "leading-5", "font-medium", "text-white")}
+        >
+          {percentage}
+        </span>
+      </div>
+      {showTooltip && (
+        <div>
+          <Tooltip
+            ref={setTooltipEle}
+            style={styles.popper}
+            content={percentage}
+            {...attributes.popper}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
