594fc7e2c5b1097c4b19f2c3c6207a35a3e089bb
ivanng-oursky <ivan.ng@oursky.com>
2022-06-24 18:25:29 +0800

    react-app: implement commission api refs oursky/likedao#169

--- react-app/src/api/distributionAPI.ts
+++ react-app/src/api/distributionAPI.ts
@@ -11,13 +11,16 @@ import { SignedTx, useCosmosAPI } from "./cosmosAPI";
 interface IDistributionAPI {
   signWithdrawDelegationRewardsTx(memo?: string): Promise<SignedTx>;
   getTotalDelegationRewards(): Promise<BigNumberCoin>;
+  getTotalCommission(): Promise<BigNumberCoin>;
 }
+
+const CoinMinimalDenom = Config.chainInfo.currency.coinMinimalDenom;
+const CoinDenom = Config.chainInfo.currency.coinDenom;
 
 export const useDistributionAPI = (): IDistributionAPI => {
   const wallet = useWallet();
   const cosmos = useCosmosAPI();
   const { query } = useQueryClient();
-  const coinMinimalDenom = Config.chainInfo.currency.coinMinimalDenom;
 
   const signWithdrawDelegationRewardsTx = useCallback(
     async (memo?: string) => {
@@ -30,7 +33,7 @@ export const useDistributionAPI = (): IDistributionAPI => {
       const rewards = await query.distribution.delegationTotalRewards(address);
 
       const relatedRewards = rewards.rewards.filter((r) =>
-        r.reward.some((rr) => rr.denom === coinMinimalDenom)
+        r.reward.some((rr) => rr.denom === CoinMinimalDenom)
       );
 
       if (relatedRewards.length === 0) {
@@ -46,7 +49,7 @@ export const useDistributionAPI = (): IDistributionAPI => {
 
       return cosmos.signTx(requests, memo);
     },
-    [coinMinimalDenom, cosmos, query, wallet]
+    [cosmos, query, wallet]
   );
 
   const getTotalDelegationRewards = useCallback(async () => {
@@ -58,7 +61,7 @@ export const useDistributionAPI = (): IDistributionAPI => {
     );
 
     const totalRewards = rewards.total.find(
-      (r) => r.denom === coinMinimalDenom
+      (r) => r.denom === CoinMinimalDenom
     );
 
     // Default cosmos decimal places is 18
@@ -67,16 +70,50 @@ export const useDistributionAPI = (): IDistributionAPI => {
     );
 
     return {
-      denom: coinMinimalDenom,
+      denom: CoinDenom,
       amount: convertMinimalTokenToToken(rewardAmount),
     };
-  }, [coinMinimalDenom, wallet, query]);
+  }, [wallet, query]);
+
+  const getTotalCommission = useCallback(async () => {
+    if (wallet.status !== ConnectionStatus.Connected) {
+      throw new Error("Wallet not connected");
+    }
+    const commission = (
+      await query.distribution.validatorCommission(wallet.account.address)
+    ).commission;
+
+    if (!commission) {
+      return {
+        denom: CoinDenom,
+        amount: convertMinimalTokenToToken(0),
+      };
+    }
+    const totalCommission = commission.commission.find(
+      (r) => r.denom === CoinMinimalDenom
+    );
+
+    // Default cosmos decimal places is 18
+    const commissionAmount = new BigNumber(
+      totalCommission?.amount ?? 0
+    ).shiftedBy(-18);
+
+    return {
+      denom: CoinDenom,
+      amount: convertMinimalTokenToToken(commissionAmount),
+    };
+  }, [wallet, query]);
 
   return useMemo(
     () => ({
       signWithdrawDelegationRewardsTx,
       getTotalDelegationRewards,
+      getTotalCommission,
     }),
-    [signWithdrawDelegationRewardsTx, getTotalDelegationRewards]
+    [
+      signWithdrawDelegationRewardsTx,
+      getTotalDelegationRewards,
+      getTotalCommission,
+    ]
   );
 };
