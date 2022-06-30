6e8dfe1bb19823f60f6f9b6d1fb178d1d7d5a44b
ivanng-oursky <ivan.ng@oursky.com>
2022-06-30 11:14:40 +0800

    react-app: refactor vote option components refs oursky/likedao#171

---
+++ react-app/src/components/ProposalVoteOption/utils.ts
@@ -0,0 +1,37 @@
+import { ProposalVoteOption } from "../../generated/graphql";
+import { MessageID } from "../../i18n/LocaleModel";
+
+export function getVoteOptionMessage(option: ProposalVoteOption): MessageID {
+  switch (option) {
+    case ProposalVoteOption.Yes:
+      return "proposal.voteOption.yes";
+    case ProposalVoteOption.Abstain:
+      return "proposal.voteOption.abstain";
+    case ProposalVoteOption.No:
+      return "proposal.voteOption.no";
+    case ProposalVoteOption.NoWithVeto:
+      return "proposal.voteOption.noWithVeto";
+    default:
+      throw new Error("Unknown vote option");
+  }
+}
+
+/**
+ * Get the tailwindCss background class name for proposal vote option
+ * @param option - proposal vote option
+ * @returns tailwind background class name
+ */
+export function getVoteOptionBgCn(option: ProposalVoteOption): string {
+  switch (option) {
+    case ProposalVoteOption.Yes:
+      return "bg-likecoin-vote-color-yes";
+    case ProposalVoteOption.Abstain:
+      return "bg-likecoin-vote-color-abstain";
+    case ProposalVoteOption.No:
+      return "bg-likecoin-vote-color-no";
+    case ProposalVoteOption.NoWithVeto:
+      return "bg-likecoin-vote-color-veto";
+    default:
+      throw new Error("Unknown vote option");
+  }
+}
