f54738b42c5efbd5bceb0c5855ab5bb6dec1945f
Elliot Ng <elliotng@oursky.com>
2022-07-11 16:05:54 +0800

    react-app: Fix js library files not resolved
    Ref oursky/likedao#143

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import resolve from "@rollup/plugin-node-resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    resolve({
+      extensions: [".js", ".ts"],
+    }),
   ],
 });
