9bc5d024b2f180621035e00d1779e198abad8508
Rico <hochiw@oursky.com>
2022-07-22 14:37:56 +0800

    react-app: Add dummy validator filter
    refs #302

--- react-app/src/components/ValidatorScreen/ValidatorScreen.tsx
+++ react-app/src/components/ValidatorScreen/ValidatorScreen.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import cn from "classnames";
+import { useSearchParams } from "react-router-dom";
 import Paper from "../common/Paper/Paper";
 import { Icon, IconType } from "../common/Icons/Icons";
 import LocalizedText from "../common/Localized/LocalizedText";
+import FilterTabs, { IFilterTabItem } from "../Tabs/FilterTabs";
+import { FilterKey } from "./ValidatorScreenAPI";
+
+type ValidatorTabItem = IFilterTabItem<FilterKey>;
+
+const defaultTabItems: ValidatorTabItem[] = [
+  {
+    label: "ValidatorScreen.filters.all",
+    value: "all",
+  },
+  {
+    label: "ValidatorScreen.filters.active",
+    value: "active",
+  },
+  {
+    label: "ValidatorScreen.filters.inactive",
+    value: "inactive",
+  },
+];
+
+const defaultTabItem = defaultTabItems[0];
 
 const ValidatorScreen: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams({
+    tab: defaultTabItem.value,
+  });
+
+  const [selectedTab] = useMemo(() => {
+    const tab = (
+      defaultTabItems.find((i) => i.value === searchParams.get("tab")) ??
+      defaultTabItem
+    ).value;
+    return [tab];
+  }, [searchParams]);
+
+  const handleSelectTab = useCallback(
+    (tab: FilterKey) => {
+      setSearchParams({
+        tab,
+      });
+    },
+    [setSearchParams]
+  );
+
   return (
-    <Paper>
+    <Paper className={cn("flex", "flex-col")}>
       <div className={cn("flex", "flex-row", "gap-x-2.5", "items-center")}>
         <Icon
           icon={IconType.Validator}
@@ -25,6 +68,13 @@ const ValidatorScreen: React.FC = () => {
         >
           <LocalizedText messageID="ValidatorScreen.title" />
         </h1>
+      </div>
+      <div className={cn("flex", "flex-col", "mt-9")}>
+        <FilterTabs<FilterKey>
+          tabs={defaultTabItems}
+          selectedTab={selectedTab}
+          onSelectTab={handleSelectTab}
+        />
       </div>
     </Paper>
   );
---
+++ react-app/src/components/ValidatorScreen/ValidatorScreenAPI.ts
@@ -0,0 +1,1 @@
+export type FilterKey = "all" | "active" | "inactive";
