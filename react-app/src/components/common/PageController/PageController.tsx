1b187e7fc54b395af4427078d639918329ce604f
Rico <hochiw@oursky.com>
2022-07-22 17:15:14 +0800

    react-app: Adjust pageController layout
    refs #302

--- react-app/src/components/common/PageController/PageController.tsx
+++ react-app/src/components/common/PageController/PageController.tsx
@@ -77,125 +77,132 @@ const PageContoller: React.FC<PageControllerProps> = (props) => {
   );
 
   return (
-    <div
-      className={cn(
-        "flex",
-        "items-center",
-        "justify-center",
-        "px-4",
-        "sm:px-0"
-      )}
-    >
-      <div
-        className={cn("-mt-px", "mr-6", "w-0", "flex-1", "flex", "justify-end")}
-      >
-        {canGoPrevious && (
-          <button
-            type="button"
-            className={cn(
-              "border-t-2",
-              "border-transparent",
-              "pt-4",
-              "inline-flex",
-              "items-center",
-              "text-sm",
-              "font-medium",
-              "text-gray-500",
-              "hover:text-gray-700",
-              "hover:border-gray-300"
-            )}
-            onClick={onPreviousClick}
-          >
-            <Icon
-              className={cn("mr-3")}
-              icon={IconType.ArrowNarrowLeft}
-              width={20}
-              height={20}
-            />
-            <LocalizedText messageID="PaginationController.previous" />
-          </button>
-        )}
-      </div>
-      <ReactPaginate
-        containerClassName={cn("flex", "items-center")}
-        pageClassName={cn("list-none")}
-        pageLinkClassName={cn(
-          "text-gray-500",
-          "hover:text-gray-700",
-          "hover:border-gray-300",
-          "hover:border-t-2",
-          "pt-4",
-          "px-4",
-          "inline-flex",
-          "items-center",
-          "text-sm",
-          "font-medium"
-        )}
-        breakLabel="..."
-        breakLinkClassName={cn(
-          "border-transparent",
-          "text-gray-500",
-          "border-t-2",
-          "pt-4",
-          "px-4",
-          "inline-flex",
-          "items-center",
-          "text-sm",
-          "font-medium",
-          "leading-[0]"
-        )}
-        activeLinkClassName={cn(
-          "hover:border-app-green",
-          "border-app-green",
-          "!text-app-green",
-          "border-t-2"
-        )}
-        pageCount={totalPage}
-        nextClassName={cn("hidden")}
-        nextLabel={null}
-        previousClassName={cn("hidden")}
-        previousLabel={null}
-        disableInitialCallback={true}
-        forcePage={currentPage}
-        onPageChange={onPageChange}
-      />
+    <div className={cn("overflow-hidden", "w-full", "h-full")}>
       <div
         className={cn(
-          "-mt-px",
-          "ml-6",
-          "w-0",
-          "flex-1",
           "flex",
-          "justify-start"
+          "items-center",
+          "justify-between",
+          "px-4",
+          "sm:px-0",
+          "overflow-x-auto"
         )}
       >
-        {canGoNext && (
-          <button
-            type="button"
-            className={cn(
-              "border-t-2",
-              "border-transparent",
-              "pt-4",
-              "inline-flex",
-              "items-center",
-              "text-sm",
-              "font-medium",
-              "text-gray-500",
-              "hover:text-gray-700",
-              "hover:border-gray-300"
-            )}
-            disabled={!canGoNext}
-            onClick={onNextClick}
-          >
-            <LocalizedText messageID="PaginationController.next" />
-            <Icon
-              className={cn("ml-3")}
-              icon={IconType.ArrowNarrowRight}
-              width={20}
-              height={20}
-            />
-          </button>
-        )}
+        <div
+          className={cn(
+            "mr-4",
+            "w-0",
+            "flex-1",
+            "flex",
+            "justify-start",
+            "sm:justify-end"
+          )}
+        >
+          {canGoPrevious && (
+            <button
+              type="button"
+              className={cn(
+                "border-t-2",
+                "border-transparent",
+                "pt-4",
+                "items-center",
+                "text-sm",
+                "font-medium",
+                "text-gray-500",
+                "hover:text-gray-700",
+                "hover:border-gray-300",
+                "inline-flex"
+              )}
+              onClick={onPreviousClick}
+            >
+              <Icon icon={IconType.ArrowNarrowLeft} width={20} height={20} />
+              <span className={cn("ml-3", "hidden", "sm:inline-block")}>
+                <LocalizedText messageID="PaginationController.previous" />
+              </span>
+            </button>
+          )}
+        </div>
+        <ReactPaginate
+          containerClassName={cn("flex", "items-center")}
+          pageRangeDisplayed={1}
+          marginPagesDisplayed={1}
+          pageClassName={cn("list-none")}
+          pageLinkClassName={cn(
+            "text-gray-500",
+            "hover:text-gray-700",
+            "hover:border-gray-300",
+            "border-t-2",
+            "border-transparent",
+            "pt-4",
+            "px-4",
+            "inline-flex",
+            "items-center",
+            "text-sm",
+            "font-medium"
+          )}
+          breakLabel="..."
+          breakLinkClassName={cn(
+            "border-transparent",
+            "text-gray-500",
+            "border-t-2",
+            "pt-4",
+            "px-4",
+            "inline-flex",
+            "items-center",
+            "text-sm",
+            "font-medium",
+            "leading-[0]"
+          )}
+          activeLinkClassName={cn(
+            "hover:border-app-green",
+            "!border-app-green",
+            "!text-app-green",
+            "border-t-2"
+          )}
+          pageCount={totalPage}
+          nextClassName={cn("hidden")}
+          nextLabel={null}
+          previousClassName={cn("hidden")}
+          previousLabel={null}
+          disableInitialCallback={true}
+          forcePage={currentPage}
+          onPageChange={onPageChange}
+        />
+        <div
+          className={cn(
+            "ml-4",
+            "w-0",
+            "flex-1",
+            "flex",
+            "justify-end",
+            "sm:justify-start"
+          )}
+        >
+          {canGoNext && (
+            <button
+              type="button"
+              className={cn(
+                "border-t-2",
+                "border-transparent",
+                "pt-4",
+                "items-center",
+                "text-sm",
+                "font-medium",
+                "text-gray-500",
+                "hover:text-gray-700",
+                "hover:border-gray-300",
+                "inline-flex"
+              )}
+              disabled={!canGoNext}
+              onClick={onNextClick}
+            >
+              <span className={cn("mr-3", "hidden", "sm:inline-block")}>
+                <LocalizedText messageID="PaginationController.next" />
+              </span>
+              <Icon icon={IconType.ArrowNarrowRight} width={20} height={20} />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
