41001685b906edc00bae400d9a362342da2b2e0c
Rico <hochiw@oursky.com>
2022-07-22 14:27:07 +0800

    react-app: Implement screen header for validator screen
    refs #302

--- react-app/src/components/ValidatorScreen/ValidatorScreen.tsx
+++ react-app/src/components/ValidatorScreen/ValidatorScreen.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import cn from "classnames";
+import Paper from "../common/Paper/Paper";
+import { Icon, IconType } from "../common/Icons/Icons";
+import LocalizedText from "../common/Localized/LocalizedText";
 
 const ValidatorScreen: React.FC = () => {
-  return <div>Hello world</div>;
+  return (
+    <Paper>
+      <div className={cn("flex", "flex-row", "gap-x-2.5", "items-center")}>
+        <Icon
+          icon={IconType.Validator}
+          fill="currentColor"
+          height={24}
+          width={24}
+          className={cn("text-app-black")}
+        />
+        <h1
+          className={cn(
+            "text-lg",
+            "leading-none",
+            "font-bold",
+            "text-app-black"
+          )}
+        >
+          <LocalizedText messageID="ValidatorScreen.title" />
+        </h1>
+      </div>
+    </Paper>
+  );
 };
 
 export default ValidatorScreen;
