90e49ba7fa7b2ff1f2d39301cc322603c2cac7b2
Rico <hochiw@oursky.com>
2022-07-28 19:43:51 +0800

    react-app: Add license and github url to footer
    refs #328

--- react-app/src/components/Footer/Footer.tsx
+++ react-app/src/components/Footer/Footer.tsx
@@ -16,57 +16,53 @@ const Footer: React.FC = () => {
           "text-sm",
           "leading-5",
           "font-normal",
+          "items-center",
           "text-gray-500",
           "desktop:flex",
-          "desktop:items-center",
+          "desktop:justify-between",
           "desktop:mt-5"
         )}
       >
-        <p className={cn("desktop:grow", "desktop:text-left")}>
-          <LocalizedText
-            messageID="footer.likecoin"
-            messageArgs={{ year: new Date().getFullYear() }}
-          />
-        </p>
-        <p
-          className={cn(
-            "mt-6",
-            "desktop:mt-0",
-            Config.footerLinks.tokenLinks.length <= 0 && "hidden"
-          )}
+        <div
+          className={cn("flex", "flex-col", "items-center", "desktop:flex-row")}
         >
-          <LocalizedText messageID="footer.getToken" />
-        </p>
+          <p className={cn("desktop:grow", "desktop:text-left")}>
+            <LocalizedText messageID="footer.license" />
+          </p>
+
+          <AppButton
+            theme="secondary"
+            size="small"
+            type="anchor"
+            href={Config.footerLinks.githubLink}
+            messageID="footer.github"
+            className={cn(
+              "text-sm",
+              "capitalize",
+              "leading-4",
+              "font-medium",
+              "text-app-darkgrey",
+              "m-3"
+            )}
+          />
+        </div>
         <div
           className={cn(
             "flex",
-            "justify-center",
+            "flex-col",
             "items-center",
-            "my-3",
-            "desktop:px-3"
+            "mt-3",
+            "desktop:flex-row",
+            "desktop:mt-0"
           )}
         >
-          {Config.footerLinks.tokenLinks.map((token) => (
-            <AppButton
-              key={token.name}
-              theme="secondary"
-              size="small"
-              type="anchor"
-              href={token.link}
-              className={cn(
-                "text-sm",
-                "capitalize",
-                "leading-4",
-                "font-medium",
-                "text-app-darkgrey",
-                "m-0.5"
-              )}
-            >
-              {token.name}
-            </AppButton>
-          ))}
-        </div>
-        {Config.footerLinks.contactSupport != null && (
+          <p
+            className={cn(
+              Config.footerLinks.tokenLinks.length <= 0 && "hidden"
+            )}
+          >
+            <LocalizedText messageID="footer.getToken" />
+          </p>
           <div
             className={cn(
               "flex",
@@ -76,24 +72,55 @@ const Footer: React.FC = () => {
               "desktop:px-3"
             )}
           >
-            <p className={cn("pr-3")}>
-              <LocalizedText messageID="footer.needHelp" />
-            </p>
-            <AppButton
-              messageID="footer.contactSupport"
-              theme="secondary"
-              size="small"
-              type="anchor"
-              href={Config.footerLinks.contactSupport}
-              className={cn(
-                "text-sm",
-                "leading-4",
-                "font-medium",
-                "text-app-darkgrey"
-              )}
-            />
+            {Config.footerLinks.tokenLinks.map((token) => (
+              <AppButton
+                key={token.name}
+                theme="secondary"
+                size="small"
+                type="anchor"
+                href={token.link}
+                className={cn(
+                  "text-sm",
+                  "capitalize",
+                  "leading-4",
+                  "font-medium",
+                  "text-app-darkgrey",
+                  "m-0.5"
+                )}
+              >
+                {token.name}
+              </AppButton>
+            ))}
           </div>
-        )}
+          {Config.footerLinks.contactSupport != null && (
+            <div
+              className={cn(
+                "flex",
+                "justify-center",
+                "items-center",
+                "my-3",
+                "desktop:px-3"
+              )}
+            >
+              <p className={cn("pr-3")}>
+                <LocalizedText messageID="footer.needHelp" />
+              </p>
+              <AppButton
+                messageID="footer.contactSupport"
+                theme="secondary"
+                size="small"
+                type="anchor"
+                href={Config.footerLinks.contactSupport}
+                className={cn(
+                  "text-sm",
+                  "leading-4",
+                  "font-medium",
+                  "text-app-darkgrey"
+                )}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
